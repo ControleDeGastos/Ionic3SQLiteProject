26fbb5dffd1ef500893a16700c00332b8223e21d
marcialwushu <marcialwushu@gmail.com>
2018-07-31 16:10:33 -0300

    setTimeout function in constructor ProviderContasPage added

--- src/providers/contas/contas.ts
+++ src/providers/contas/contas.ts
@@ -1,5 +1,6 @@
-//import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import 'rxjs/add/operator/map';
+import { SQLite, SQLiteObject } from '@ionic-native/sqlite';
 
 @Injectable()
 export class ContasProvider {
@@ -7,7 +8,22 @@ export class ContasProvider {
   list:any = [];
 
   constructor() {
-
+    setTimeout(function() {
+      this.database = new SQLite();
+      let createSql = "CREATE TABLE IF NOT EXISTS contas (id INTEGER PRIMARY KEY AUTOINCREMENT, descricao TEXT)";
+      this.database.openDatabase({
+        name: "data.db",
+        location: "default"
+      }).then(() => {
+        this.database.executeSql(createSql, {}).then((data) => {
+          console.log("Tabela contas criada: ", data);
+        }, (error) => {
+          console.error("Erro na execução da criação da tabela contas", error);
+        })
+      }, (error) => {
+        console.error("Erro na abertura do banco de dados: contas", error);
+      });
+    }, 600);
   }
 
   getList(){
@@ -20,7 +36,12 @@ export class ContasProvider {
   }
 
   insert(conta, successCallBack){
+    setTimeout(function() {
+      let sqlQuery = "INSERT INTO contas (descricao) VALUES (?)";
+      this.database.executeSql(sqlQuery, [conta.descricao]).then((data) => {
 
+      })
+    })
   }
 
   edit(conta){
