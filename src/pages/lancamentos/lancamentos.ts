a24ea5c7561358beb46ce7861b8a01190ea0e4df
marcialwushu <marcialwushu@gmail.com>
2018-08-21 17:04:17 -0300

    lancamentoProvider functions

--- src/pages/lancamentos/lancamentos.ts
+++ src/pages/lancamentos/lancamentos.ts
@@ -1,7 +1,8 @@
+import { ModalContasPage } from './../modal-contas/modal-contas';
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { DatabaseProvider } from '../../providers/database/database';
+import { IonicPage, NavController, NavParams, ModalController, Toast, ToastController } from 'ionic-angular';
 import { SQLiteObject } from '@ionic-native/sqlite';
+import { ContasProvider } from '../../providers/contas/contas';
 
 
 @IonicPage()
@@ -14,24 +15,20 @@ export class LancamentosPage {
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
-    private dbProvider: DatabaseProvider
+    private providerContas: ContasProvider,
+    public modalCtrl: ModalController,
+    private toast: ToastController
 
   ) {
 
-    dbProvider.criarDatabase();
+
   }
 
-  insert(lancamento:Lancamentos){
-    return this.dbProvider.getDB()
-    .then((db: SQLiteObject) => {
-      let sql = 'insert into lancamento (descricao, valor, data, conta, entradaSaida, pago) values (?, ?, ?, ?, ?, ?)';
-      let data = [lancamento.descricao];
-      return db.executeSql(sql, data)
-
-      .catch((e) => console.error(e));
-    })
-    .catch((e) => console.error(e));
+  insert(){
+    let modal = this.modalCtrl.create(ModalContasPage)
   }
+
+
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad LancamentosPage');
@@ -39,12 +36,3 @@ export class LancamentosPage {
 
 }
 
-export class Lancamentos{
-  id: number;
-  descricao:string;
-  valor:number;
-  data: string;
-  conta: string;
-  entradaSaida: string;
-  pago: string;
-}
--- src/providers/lancamentos/lancamentos.ts
+++ src/providers/lancamentos/lancamentos.ts
@@ -1,17 +1,42 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { DatabaseProvider } from '../database/database';
+import { SQLiteObject } from '@ionic-native/sqlite';
 
-/*
-  Generated class for the LancamentosProvider provider.
 
-  See https://angular.io/guide/dependency-injection for more info on providers
-  and Angular DI.
-*/
 @Injectable()
 export class LancamentosProvider {
 
-  constructor(public http: HttpClient) {
-    console.log('Hello LancamentosProvider Provider');
+  constructor(private dbProvider: DatabaseProvider) {
+    dbProvider.criarDatabase();
+  }
+
+  insert(lancamento:Lancamentos){
+    return this.dbProvider.getDB()
+    .then((db: SQLiteObject) => {
+      let sql = 'insert into lancamento (descricao, valor, data, conta, entradaSaida, pago) values (?, ?, ?, ?, ?, ?)';
+      let data = [lancamento.descricao];
+      return db.executeSql(sql, data)
+
+      .catch((e) => console.error(e));
+    })
+    .catch((e) => console.error(e));
+  }
+
+  edit(conta){
+
   }
 
 }
+
+export class Lancamentos{
+  id: number;
+  descricao:string;
+  valor:number;
+  data: string;
+  conta: string;
+  entradaSaida: string;
+  pago: string;
+}
+
+
+
