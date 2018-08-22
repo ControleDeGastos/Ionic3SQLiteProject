f84f94b055f9bf92796047b37be40aa8456ad8a4
marcialwushu <marcialwushu@gmail.com>
2018-08-22 11:21:48 -0300

    ModalLancamentosPage html and typescript variables added

--- src/pages/modal-lancamentos/modal-lancamentos.ts
+++ src/pages/modal-lancamentos/modal-lancamentos.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Lancamentos } from '../../providers/lancamentos/lancamentos';
 
 
 @IonicPage()
@@ -9,11 +10,27 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class ModalLancamentosPage {
 
+  view: any;
+
+  classe: Lancamentos;
+
+  contas: any[] = [];
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ModalLancamentosPage');
+  }
+
+
+
+  cancel(){
+
+  }
+
+  salvar(){
+
   }
 
 }
